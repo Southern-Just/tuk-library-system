c77f4c7f242b78a86aeb96d6dacb56d596d92c74
Southern-Just <karuguezekiel@gmail.com>
2025-02-11 07:58:45 +0300

    before pulling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import HeroCover from "@/components/HeroCover";
 
-const Hero = ({title,description,author,total_books,available_books,cover}: Book_Meta) => {
+const Hero = ({
+    id,
+    title,
+    description,
+    author,
+    total_books,
+    available_books,
+    coverImage}: Book_Meta) => {
     return (
         <section className="hero">
         <div className="hero-text">
-            <p className="hero-text-h1">{title}</p>
+            <h1 className="hero-text-h1">{title}</h1>
             <p className="hero-text-description">{description}</p>
             <div className="hero-text-add">
                 <p >author: {author}</p>
@@ -13,10 +20,10 @@ const Hero = ({title,description,author,total_books,available_books,cover}: Book
                 <p>Available:<span> {available_books}</span></p>
             </div>
         </div>
-            <HeroCover
-                variant='full'
-                className='z-5'
-                coverImage={cover}/>
+                    <HeroCover
+                        variant='full'
+                        className='z-5'
+                        coverImage={coverImage}/>
       </section>
     );
 }
